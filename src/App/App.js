5f4f87070d5478116c2304b5c621fd0f013b2f82
Brian Baugher <123590980+brian-baugher@users.noreply.github.com>
2023-07-23 21:42:14 -0400

    begin overhaul

---
+++ src/App/App.js
@@ -0,0 +1,24 @@
+import style from './App.module.css';
+
+function App() {
+  return (
+    <div className={style.App}>
+      <header className={style.App_header}>
+        <img className={style.App_logo} alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className={style.App_link}
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
