b18f6d747117b6b8f802a4d62cfedae711be8137
Brian Baugher <bdb7hq@virginia.edu>
2023-08-01 00:37:03 -0400

    add images to panels in projects

--- src/Projects/Panel/Panel.js
+++ src/Projects/Panel/Panel.js
@@ -3,15 +3,15 @@ import style from './Panel.module.css'
 export default function Panel({ priority, image, title, description, links }){
     function getClass(){
         switch(priority){
-            case 'first': return style.first;
-            case 'second': return style.second;
-            case 'third': return style.third;
+            case 'first': return `${style.first} ${style.panel}`;
+            case 'second': return `${style.second} ${style.panel}`;
+            case 'third': return `${style.third} ${style.panel}`;
         }
     }
 
     return (
-        <div className={getClass()}>
-            
+        <div className={getClass()} style={{ backgroundImage: `url(${image})` }}>
+            <p>hello</p>
         </div>
     )
 }
--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -1,5 +1,8 @@
 import Panel from './Panel/Panel'
 import style from './Projects.module.css'
+import mathesis from './32021370.PNG';
+import hackation from './3202180.PNG';
+import portfolio from './bdb08.PNG'
 
 export default function Projects(){
     return(
@@ -7,11 +10,11 @@ export default function Projects(){
             <strong>Projects</strong>
             <div className={style.panels}>
                 <div className={style.top}>
-                    <Panel priority={'first'}/>
+                    <Panel priority={'first'} image={mathesis}/>
                 </div>
                 <div className={style.bottom}>
-                    <Panel priority={'second'}/>
-                    <Panel priority={'third'}/>
+                    <Panel priority={'second'} image={hackation}/>
+                    <Panel priority={'third'} image={portfolio}/>
                 </div>
             </div>
         </div>
