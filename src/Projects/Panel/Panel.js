b23064ad49318359f8fca06179ca8acbb3524fac
Brian Baugher <bdb7hq@virginia.edu>
2023-08-02 14:25:15 -0400

    fill in xTensiv project panel

--- src/Projects/Panel/Panel.js
+++ src/Projects/Panel/Panel.js
@@ -1,6 +1,6 @@
 import style from './Panel.module.css'
 
-export default function Panel({ priority, image, title, description, links }){
+export default function Panel({ priority, image, title, description, links, tech_stack, hidden=false }){
     function getClass(){
         switch(priority){
             case 'first': return `${style.first} ${style.panel}`;
@@ -12,14 +12,24 @@ export default function Panel({ priority, image, title, description, links }){
     return (
         <div className={getClass()} style={{ backgroundImage: `url(${image})` }}>
             <div className={style.column_container}>
-                <h1>Title</h1>
-                <p>Ut tempor est nulla adipisicing occaecat ipsum nostrud exercitation nulla proident consectetur dolore quis irure.</p>
-                <p>tech stack</p>
+                <h1>{title}</h1>
+                <p>{description}</p>
+                <p>{tech_stack}</p>
             </div>
             <div className={`${style.column_container} ${style.link_container}`}>
                 <div className={style.links}>
-                    <a>demo</a>
-                    <a>code</a>
+                    {hidden?
+                    <>
+                        <a href={links[0]} target='_blank'>Live Site</a>
+                        <a>Private project, contact for code samples / login</a>
+                    </>
+                    :
+                    <>
+                        <a href={links[0]} target='_blank'>demo</a>
+                        <a href={links[1]} target='_blank'>code</a>
+                    </>
+                    }
+                    
                 </div>
             </div>
         </div>
--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -7,6 +7,13 @@ import { useRef } from 'react';
 import '../animate.css'
 import { useObserver } from '../hooks';
 
+let xTensivTitle = 'xTensiv.io (a Mathesis project)'
+let xTensivDescription = "Full featured React web app built to help math learners navigate and study a large libary of mathematics represented in a vast directed graph. "+
+"Built as a MVP to pilot the application before seeking investors, deployed to over 100 students in select math classes at UVA. "+
+"Features tools to help teachers create content including: markdown/katex math support, auto-save functionality, and graph data upload/download."
+let xTensivTechStack = "React | react-router | recoil | cytoscape.js | firebase hosting/auth";
+let xTensivLinks = ['https://www.xTensiv.io/', '']
+
 export default function Projects(){
     const panel1 = useRef()
     const panel2 = useRef()
@@ -20,11 +27,15 @@ export default function Projects(){
             <strong className={style.title}>Projects</strong>
             <div className={style.panels}>
                 <div className={style.top}>
-                    <div className={`hidden`} ref={panel1}><Panel priority={'first'} image={mathesis}/></div>
+                    <div className={`hidden`} ref={panel1}><Panel priority={'first'} image={mathesis}
+                    title={xTensivTitle} description={xTensivDescription} tech_stack={xTensivTechStack} 
+                    hidden links={xTensivLinks}/></div>
                 </div>
                 <div className={style.bottom}>
-                    <div className={`${style.wrapper1} hidden`} ref={panel2}><Panel priority={'second'} image={hackation}/></div>
-                    <div className={`${style.wrapper2} hidden`} ref={panel3}><Panel priority={'third'} image={portfolio}/></div>
+                    <div className={`${style.wrapper1} hidden`} ref={panel2}>
+                        <Panel priority={'second'} image={hackation} links={[2,2]}/></div>
+                    <div className={`${style.wrapper2} hidden`} ref={panel3}>
+                        <Panel priority={'third'} image={portfolio} links={[2,2]}/></div>
                 </div>
             </div>
         </div>
